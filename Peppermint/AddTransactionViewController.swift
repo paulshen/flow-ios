941bf3789386f4fdac3dc8c19b76a2dd0d6a86c7
Paul Shen <paulshen0@gmail.com>
2014-11-12 15:47:56 -0800

    Make text inputs tabbable

--- Peppermint/AddTransactionViewController.swift
+++ Peppermint/AddTransactionViewController.swift
@@ -19,21 +19,33 @@ class AddTransactionViewController: UIViewController {
   @IBOutlet weak var cancelButton: UIButton!
 
   var swipeDownRecognizer: UISwipeGestureRecognizer!
+  var tapRecognizer: UITapGestureRecognizer!
   
   override func viewDidLoad() {
     descriptionInput.textContainer.lineFragmentPadding = 0
     descriptionInput.textContainerInset = UIEdgeInsetsZero
+    descriptionInput.tag = 1
+    priceInput.tag = 2
+    descriptionInput.delegate = self
+    priceInput.delegate = self
     
     saveButton.addTarget(self, action: Selector("onSaveButtonPressed:"), forControlEvents: UIControlEvents.TouchUpInside)
     cancelButton.addTarget(self, action: Selector("onCancelButtonPressed:"), forControlEvents: UIControlEvents.TouchUpInside)
     
-    swipeDownRecognizer = UISwipeGestureRecognizer(target: self, action: "onSwipeDown:")
+    swipeDownRecognizer = UISwipeGestureRecognizer(target: self, action: Selector("onSwipeDown:"))
     swipeDownRecognizer.direction = .Down
     view.addGestureRecognizer(swipeDownRecognizer)
+    
+    tapRecognizer = UITapGestureRecognizer(target: self, action: Selector("onTap:"))
+    view.addGestureRecognizer(tapRecognizer)
   }
   
   func onSwipeDown(sender: UISwipeGestureRecognizer) {
     presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  func onTap(sender: UITapGestureRecognizer) {
+    view.endEditing(true)
   }
   
   func onSaveButtonPressed(sender: UIButton!) {
@@ -49,5 +61,33 @@ class AddTransactionViewController: UIViewController {
   
   func onCancelButtonPressed(sender: UIButton!) {
     presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+  }
+}
+
+extension AddTransactionViewController: UITextViewDelegate, UITextFieldDelegate {
+  func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+    if NSString(string: text).rangeOfCharacterFromSet(NSCharacterSet.newlineCharacterSet()).location == NSNotFound {
+      return true
+    }
+    
+    let nextTag = textView.tag + 1
+    let nextResponder = textView.superview?.viewWithTag(nextTag)
+    if let nextResponder = nextResponder {
+      nextResponder.becomeFirstResponder()
+    } else {
+      textView.resignFirstResponder()
+    }
+    return false
+  }
+  
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    let nextTag = textField.tag + 1
+    let nextResponder = textField.superview?.viewWithTag(nextTag)
+    if let nextResponder = nextResponder {
+      nextResponder.becomeFirstResponder()
+    } else {
+      textField.resignFirstResponder()
+    }
+    return false
   }
 }
