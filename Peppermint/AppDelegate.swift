7216ced4f01976165e99568d8a661c837db8fc55
Paul Shen <paulshen0@gmail.com>
2014-11-17 15:06:09 -0800

    Move Parse keys to untracked Config

--- Peppermint/AppDelegate.swift
+++ Peppermint/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
     
-    Parse.setApplicationId("***REMOVED***", clientKey: "***REMOVED***")
+    Parse.setApplicationId(Config.ParseApplicationID, clientKey: Config.ParseClientKey)
     
     self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
     let rootVC = UINavigationController()
